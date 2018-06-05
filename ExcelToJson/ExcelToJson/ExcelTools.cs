5a5779c1d194236a82622cfa664dacc5d57ab125
minstrelLJ <minstrelLJ@163.com>
2018-06-05 12:45:17 +0800

    add excelToJson

---
+++ ExcelToJson/ExcelToJson/ExcelTools.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ExcelToJson
+{
+    public class ExcelTools
+    {
+        /// <summary>
+        /// Excel文件列表
+        /// </summary>
+        private static List<string> excelList;
+
+        public static void Start()
+        {
+            LoadExcel();
+            Convert();
+        }
+
+        private static void LoadExcel()
+        {
+            if (excelList == null) excelList = new List<string>();
+            excelList.Clear();
+
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] fil = dir.GetFiles();
+
+            string fiPath = "";
+            foreach (var fi in fil)
+            {
+                if (fi.Extension == ".xlsx")
+                {
+                    fiPath = fi.FullName.Replace("\\", "/");
+                    excelList.Add(fiPath);
+                }
+            }
+        }
+
+         //<summary>
+         //转换Excel文件
+         //</summary>
+        private static void Convert()
+        {
+            foreach (string assetsPath in excelList)
+            {
+                //构造Excel工具类
+                ExcelUtility excel = new ExcelUtility(assetsPath);
+
+                //编码类型
+                Encoding encoding = Encoding.GetEncoding("utf-8");
+
+                string output = assetsPath.Replace(".xlsx", ".json");
+                excel.ConvertToJson(output, encoding);
+            }
+        }
+    }
+}
---
+++ ExcelToJson/ExcelToJson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ExcelTools.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.TargetSite);
+                Console.ReadLine();
+            }
+        }
+    }
+}
